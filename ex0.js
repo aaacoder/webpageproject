471ed852c632b7a047f96b66a5905ef3a23883e5
adidam23 <aditi.dam@gmail.com>
2021-10-20 10:55:51 -0400

    commit

---
+++ ex0.js
@@ -0,0 +1,12 @@
+console.log("Program is starting!");
+alert('Hello World!');
+
+// Greet the user
+let message = "Hello User!";
+alert(message);
+
+let name = prompt("Actually, what is your name?");
+// let name = prompt("Actually, what is your name?", "Bob");    2nd param is default name
+let newMessage = "Greetings, " + name + ".";
+alert(newMessage);
+console.log("Program has ended.");
